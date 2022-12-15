c53c2a58f3748b3f6546fc207382fe8f729dd4e5
LudovicLefieux <lefieux.ludovic@protonmail.com>
2022-12-15 14:42:02 +0100

    Adding ContactList component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   return (
     <div className="App">
-      <Contact 
-        name="Roberto Garcia" 
-        avatar="https://randomuser.me/api/portraits/men/55.jpg"
-        online
-      />
-      <Contact 
-        name="Lewis Steward" 
-        avatar="https://randomuser.me/api/portraits/men/47.jpg"
-      />
-      <Contact 
-        name="Julie Kennedy" 
-        avatar="https://randomuser.me/api/portraits/women/52.jpg"
-        online
-      />
+      <ContactList />
     </div>
   );
 }
