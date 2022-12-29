be492eabadd0a79e0a718ab041398504ce91ec1d
Ludovic Lefieux <lefieux.ludovic@protonmail.com>
2022-12-29 14:23:10 +0100

    Adding online/offline status switch feature in adding state to the Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,44 @@
-import PropTypes from 'prop-types';
-import './Contact.css';
+import PropTypes from "prop-types";
+import React from "react";
+import "./Contact.css";
 
-function Contact(props) {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: props.online,
+    };
+  }
+
+  render() {
     return (
-        <div className='Contact'>
-            <img className='avatar' src={ props.avatar } alt='User profile'/>
-            <div>
-                <span className='name'>{ props.name }</span>
-                <div className='status'>
-                    <span className={ props.online ? 'status-online' : 'status-offline' }></span>
-                    <span className='status-text'>{ props.online ? 'online' : 'offline' }</span>
-                </div>
-            </div>
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar} alt="User profile" />
+        <div>
+          <span className="name">{this.props.name}</span>
+          <div
+            className="status"
+            onClick={(event) => {
+              const isOnline = !this.state.online;
+              this.setState({ online: isOnline });
+            }}
+          >
+            <span
+              className={this.state.online ? "status-online" : "status-offline"}
+            ></span>
+            <span className="status-text">
+              {this.state.online ? "online" : "offline"}
+            </span>
+          </div>
         </div>
+      </div>
     );
+  }
 }
 
 Contact.propTypes = {
-    name: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
 };
 
 export default Contact;
